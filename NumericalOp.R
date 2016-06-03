8b1f8603b46eedb63f587ddae0078e16320550cb
tmyst <tomoya.saitoh@gmail.com>
2016-06-03 11:15:14 +0900

    function name changed

--- NumericalOp.R
+++ NumericalOp.R
@@ -1,8 +1,9 @@
 # IEEE 754 最近接丸め（0から遠いほうへ）
 # 通常日本で最初に習う四捨五入
-round_5in <- function(x, digits=0){
+
+round_5up <- function(x, digits=0){
   if(floor(x*10^(digits+1)) %% 10 == 5){
-    round(x+sign(x)*10^(-(digits+1)), digits)
+    round(x+sign(x)*10^(-(digits+1)), digits) # 最後の桁を1だけ増やしてround
   }else{
     round(x, digits)
   }
